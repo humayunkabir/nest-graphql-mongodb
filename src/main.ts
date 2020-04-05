7df42d48664495f0c42322d356a47f09f84385c3
Humayun kabir <humayunkabir.cep@gmail.com>
2020-04-05 19:46:19 +0600

    Support port

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
 }
 bootstrap();
